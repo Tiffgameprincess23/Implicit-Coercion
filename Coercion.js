83fcf69f7844503513f4e631a9a9ecef3b66c6c1
Tiffany Stamp <136510743+Tiffgameprincess23@users.noreply.github.com>
2023-07-28 10:21:45 -0600

    Create Coercion.js

---
+++ Coercion.js
@@ -0,0 +1,12 @@
+let num = 10
+let str = "5"
+let result = num + str
+
+let strnum = "50"
+let convertedNum = Number()
+let convertedStr = 50 * 2
+
+num = 0
+str = "0";
+console.log(num == str);
+console.log(num === str)
